0410a66d9e4b973150a8f975ce50dd1a8cb4ee94
himself65 <himself6565@gmail.com>
2019-02-02 11:43:36 +0800

    [add] types

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+export const metaRegex: RegExp
+export const metaAllRegex: RegExp
+export const metaLeftRegex: RegExp
+export const metaRightRegex: RegExp
+
+export function removeMeta (data: string): string
+
+export function parseMeta (data: string): {}
